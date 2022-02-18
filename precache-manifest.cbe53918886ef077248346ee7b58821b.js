432abc83e34bd03b1c9095f3e906f3d7870d3e46
unknown <hsantamariaca@unprg.edu.pe>
2022-02-18 01:39:00 -0500

    Despliegue

---
+++ precache-manifest.cbe53918886ef077248346ee7b58821b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee110823f99cde1bd5f",
+    "url": "/Binary/css/app.d224dbcd.css"
+  },
+  {
+    "revision": "c1f86f2e611b7100cc20",
+    "url": "/Binary/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "beedb25e3e5be72196ac09c2878e7856",
+    "url": "/Binary/img/assets/icomoon/fonts/icomoon.eot"
+  },
+  {
+    "revision": "eabca1ec9e7b746294a2e03cd383be50",
+    "url": "/Binary/img/assets/icomoon/fonts/icomoon.svg"
+  },
+  {
+    "revision": "2a268d658d02b4734aebd71a4fda272c",
+    "url": "/Binary/img/assets/icomoon/fonts/icomoon.ttf"
+  },
+  {
+    "revision": "2c55132e674bf6a40a458247f56395e5",
+    "url": "/Binary/img/assets/icomoon/fonts/icomoon.woff"
+  },
+  {
+    "revision": "1bf6c422f45390e1fd292beec541ce73",
+    "url": "/Binary/img/assets/icomoon/style.css"
+  },
+  {
+    "revision": "9c6f63de859ba7a052268068c30ba45f",
+    "url": "/Binary/index.html"
+  },
+  {
+    "revision": "1ee110823f99cde1bd5f",
+    "url": "/Binary/js/app.8be71539.js"
+  },
+  {
+    "revision": "c1f86f2e611b7100cc20",
+    "url": "/Binary/js/chunk-vendors.2d522e69.js"
+  },
+  {
+    "revision": "89081d7a6af037c4db4b4538af2f5aec",
+    "url": "/Binary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Binary/robots.txt"
+  }
+]);
